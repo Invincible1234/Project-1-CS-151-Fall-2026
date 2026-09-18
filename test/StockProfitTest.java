5d36b0f66b2068d8f9c1ca400d931bc9f47bb482
Angelica <69700858+ang-pb@users.noreply.github.com>
2026-09-17 18:11:39 -0700

    Added my test cases to StockProfitTest.java

--- test/StockProfitTest.java
+++ test/StockProfitTest.java
@@ -20,5 +20,21 @@ public class StockProfitTest {
 
         int[] prices5 = {1};
         System.out.println(s.maxProfit(prices5) == 0);
+
+        //Angelica's Test Cases
+        int[] prices6 = {3, 3, 5, 0, 0, 3, 1, 4};
+        System.out.println(s.maxProfit(prices6) == 8);
+
+        int[] prices7 = {1, 2, 1, 2, 1, 2};
+        System.out.println(s.maxProfit(prices7) == 3);
+        
+        int[] prices8 = {5, 5, 5, 5};
+        System.out.println(s.maxProfit(prices8) == 0);
+
+        int[] prices9 = {2, 1};
+        System.out.println(s.maxProfit(prices9) == 0);
+
+        int[] prices10 = {1, 2};
+        System.out.println(s.maxProfit(prices10) == 1);
     }
 }
